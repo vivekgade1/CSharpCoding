fe1e598cb250276a56d890d611108bc85a57cdbc
vgade <vgade@tesla.com>
2018-08-08 22:39:55 -0700

    Initial Setup

---
+++ CodePractice/CodePractice/Commons/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePractice.Commons
+{
+
+    /// <summary>
+    /// Linked List Node.
+    /// </summary>
+    class ListNode
+    {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { this.val = x; this.next = null;
+    }
+
+    class CreateLinkedList
+    {
+            public CreateLinkedList(int[] input)
+            {
+
+            }
+
+    }
+ }
+}
---
+++ CodePractice/CodePractice/LinkedList/List2Pointer.cs
@@ -0,0 +1,49 @@
+﻿using CodePractice.Commons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodePractice.LinkedList
+{
+    /// <summary>
+    /// Contains programes related to List 2 pointer section 
+    /// @author vgade
+    /// </summary>
+    class List2Pointer
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(LPalin(new ListNode(1)));
+        }
+        private static int LPalin(ListNode inputList)
+        {
+            if (inputList == null) return 0; // check for null.
+            
+            Stack<int> poppedList = new Stack<int>();
+            ListNode head = inputList;
+            ListNode headToCheck = inputList;
+            // Traverse the list to push the values into this Stack
+            while (head.next != null)
+            {
+                poppedList.Push(head.val);
+                head = head.next;
+            }
+
+            // Match the elements for checking the palindrome. 
+            while(headToCheck.next != null)
+            {
+                int popedElement = poppedList.Pop();
+                if(popedElement == headToCheck.val)
+                {
+                    headToCheck = headToCheck.next;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
